afbb41e7f76f4ebf1a8d3cf9c70b96824f9260ea
Gabriele Filaferro <gabriele.filaferro@gmail.com>
2020-04-18 19:39:38 +0200

    Removed shrink header on desktop

--- static/index.js
+++ static/index.js
@@ -1,14 +1,15 @@
-window.onscroll = function () {
-  scrollFunction();
-};
-
-// TODO need to adjust based on SASS variables
-function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.querySelector('header').style.height = '60px';
-    document.querySelector('label').style.top = '10px';
-  } else {
-    document.querySelector('header').style.height = '80px';
-    document.querySelector('label').style.top = '22px';
-  }
+if (isMobile) {
+  window.onscroll = () => {
+    // TODO need to adjust based on SASS variables
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      document.querySelector('header').style.height = '60px';
+      document.querySelector('label').style.top = '10px';
+    } else {
+      document.querySelector('header').style.height = '80px';
+      document.querySelector('label').style.top = '22px';
+    }
+  };
 }
