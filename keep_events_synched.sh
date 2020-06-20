73c6bd66c3d0621996b691c55d7814287357b088
Gabriele Filaferro <gabriele.filaferro@gmail.com>
2020-06-20 23:42:05 +0200

    Added github action to keep events in sych

---
+++ keep_events_synched.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+# this script will run every night with github action
+# it is for moving automatically the events from future to past
+# it will redeploy the site only if there is an event on yesterday
+
+# get yesterday timestamp
+YESTERDAY=$(date -d "-1 days" +'%Y%m%d')
+
+# get the files of the events
+EVENT_FILES=content/eventi/*.md
+
+# for each event check if one is of yesterday
+for f in $EVENT_FILES
+do
+    f=$(basename -- "$f")
+    f=${f%.md}
+    if [ "$f" = "index" ]
+    then
+    echo doing nothing because file is index
+    # if one event is of yesterday trigger redeploy
+    elif [ "$f" -eq "$YESTERDAY" ]
+    then
+        curl -X POST -d {} $DEPLOY_WEB_HOOK
+    fi
+done
