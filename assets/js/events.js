3212c780ed435fd05cced452cbc3e6bc6f24526c
Gabriele Filaferro <gabriele.filaferro@gmail.com>
2020-10-10 20:47:14 +0200

    Fixed icon behavior in event card on mobile

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,15 +1,22 @@
-const moreBtns = document.querySelectorAll('.flaticon-more');
+const actionBtns = document.querySelectorAll('.action-btn');
+const cancelBtns = document.querySelectorAll('.cancel');
 const eventsDescription = document.querySelectorAll('.eventCard p');
 const eventsImgs = document.querySelectorAll('.eventCard picture');
 
-moreBtns.forEach((btn, idx) => {
+actionBtns.forEach((btn, idx) => {
   btn.addEventListener('click', e => {
     if (window.getComputedStyle(eventsDescription[idx]).display == 'none') {
+      // the event is close
       eventsDescription[idx].style.display = 'block';
       eventsImgs[idx].style.display = 'block';
+      actionBtns[idx].classList.remove('flaticon-more');
+      actionBtns[idx].classList.add('flaticon-cancel');
     } else {
+      // the event is open
       eventsDescription[idx].style.display = 'none';
       eventsImgs[idx].style.display = 'none';
+      actionBtns[idx].classList.remove('flaticon-cancel');
+      actionBtns[idx].classList.add('flaticon-more');
     }
   });
 });
